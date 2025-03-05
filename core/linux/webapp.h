bbf481b4acedf60f0f124c801d7f96f6c2a1cd1d
ty <30928898+yincolor@users.noreply.github.com>
2025-03-05 21:47:22 +0800

    增加extern 函数，用于展示

--- core/linux/webapp.h
+++ core/linux/webapp.h
@@ -3,6 +3,12 @@
 
 #include"api.h"
 
+extern WebApp *webapp_create(char *app_name, uint32_t version, char *title, char *uri, uint8_t is_debug);
+extern void webapp_run(WebApp *webapp); 
+extern void webapp_switch_fullscreen(WebApp *webapp); 
+extern void webapp_set_icon_by_svg_bytes(WebApp *webapp, uint8_t *icon_file_bytes, uint64_t icon_file_size);
+extern void webapp_set_user_agent(WebApp *webapp, char *user_agent);
+
 #endif
 
 
